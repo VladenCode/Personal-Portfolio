591941c5088819708ec5919de5cd4ccea4d615af
VladenCode <bladekknight@gmail.com>
2022-12-13 20:28:46 +0100

    changes in footer section, added colors to idividual projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,20 @@
 // Scroll and change hero color (OLD MEG HOGZ CSAK DESKTOP LEGYEN)
 
 const body = document.getElementsByTagName('body')[0];
-const title = document.getElementsByTagName('h1')[0];
-const pharagraph = document.getElementsByTagName('p')[0];
-const navLink = document.getElementsByTagName('li')[0];
+const nav = document.getElementsByClassName('hover')[0];
 const logo = document.querySelector('#nav_logo-change');
-
-// starting
-body.style.backgroundColor = 'white';
-title.style.color = 'black';
-pharagraph.style.color = 'black';
-navLink.style.color = 'black';
-logo.src = './assets/img/logo-black.png';
 
 // trigger this function every time the user scrolls
 window.onscroll = function (event) {
   var scroll = window.pageYOffset;
-  if (scroll < 300) {
-    body.style.backgroundColor = 'white';
-  } else if (scroll >= 300 && scroll < 400) {
-    body.style.backgroundColor = 'white';
-    title.style.color = 'black';
-    pharagraph.style.color = 'black';
-    navLink.style.color = 'black';
-    logo.src = './assets/img/logo-black.png';
-  } else {
-    body.style.backgroundColor = 'black';
-    title.style.color = 'white';
-    pharagraph.style.color = 'white';
-    navLink.style.color = 'white';
+
+  if (scroll >= 300) {
+    body.classList.remove('colorChange');
+    nav.classList.remove('hover');
     logo.src = './assets/img/logo.png';
+  } else {
+    body.classList.add('colorChange');
+    nav.classList.add('hover');
+    logo.src = './assets/img/logo-black.png';
   }
 };
